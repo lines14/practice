f94f9600097941263facee23fd242e8968c22880
Lines14 <tonybass1414@yahoo.com>
2022-10-24 04:43:31 +0600

    Fix path missmatching

--- Excel script Moysklad.py
+++ Excel script Moysklad.py
@@ -1,7 +1,7 @@
-with open('/home/lines14/projects/practice/modifications.txt', 'r') as file_1:
+with open('modifications.txt', 'r') as file_1:
     f1 = file_1.readlines()
 
-with open('/home/lines14/projects/practice/codes.txt', 'r') as file_2:
+with open('codes.txt', 'r') as file_2:
     f2 = file_2.readlines()
 
 spis = []
@@ -20,5 +20,5 @@ uniq_and_fifa = dict(zip(spis, spis_2))
 for i in uniq_and_fifa.items():
     for j in i[1]:
         kok = i[0]+'\t'+j+'\n'
-        with open('/home/lines14/projects/practice/final.txt', 'a') as kek:
+        with open('final.txt', 'a') as kek:
             kek.write(kok)
