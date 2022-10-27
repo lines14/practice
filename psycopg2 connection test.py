391613df305d58f81bedbe6b8c265694d407aa30
Lines14 <tonybass1414@yahoo.com>
2022-10-28 04:50:55 +0600

    Fix sshtunnel port

--- psycopg2 connection test.py
+++ psycopg2 connection test.py
@@ -2,11 +2,9 @@ import paramiko
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
 client.connect('192.168.1.215', 2222, username='lines14', key_filename='/home/lines14/.ssh/id_ed25519')
 
 stdin, stdout, stderr = client.exec_command('ls -a')
-
 print(stdout.readlines())
 
 
@@ -18,8 +16,7 @@ with SSHTunnelForwarder(
 ssh_pkey="/home/lines14/.ssh/id_ed25519",
 ssh_private_key_password="106107",
 ssh_username="lines14",
-remote_bind_address=('127.0.0.1', 8080)) as server:
-    print(server.local_bind_port)
+remote_bind_address=('127.0.0.1', 5432)) as server:
 
     conn = psycopg2.connect(user='admindb', password='106107', dbname="newdatabase", host="127.0.0.1", port="5432")
     curs = conn.cursor()
