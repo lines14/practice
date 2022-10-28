2c6b85233a2187c085013f4949ebea6154c11ca8
Lines14 <tonybass1414@yahoo.com>
2022-10-28 19:56:33 +0600

    Chg script to psycopg2 connector

---
+++ Psycopg2 remote DB connector.py
@@ -0,0 +1,24 @@
+from sshtunnel import SSHTunnelForwarder
+
+server = SSHTunnelForwarder(
+    ('192.168.1.215', 2222),
+    ssh_username='lines14',
+    ssh_pkey="/home/lines14/.ssh/id_ed25519",
+    remote_bind_address=('127.0.0.1', 5432),
+    local_bind_address=('127.0.0.1', 43807))
+
+server.start()
+
+import psycopg2
+conn = psycopg2.connect(
+    database="newdatabase",
+    user='admindb',
+    host=server.local_bind_host,
+    port=server.local_bind_port,
+    password='106107')
+cur = conn.cursor()
+cur.execute("select * from table1;")
+data = cur.fetchall()
+print(data)
+
+server.stop()
