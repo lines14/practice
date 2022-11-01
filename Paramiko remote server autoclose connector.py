71ab978efe624dff8e86ce1b16f9f9fe50d17727
Lines14 <tonybass1414@yahoo.com>
2022-11-01 17:58:21 +0600

    Add "closing" function to paramiko connector

--- Paramiko remote server autoclose connector.py
+++ Paramiko remote server autoclose connector.py
@@ -1,10 +1,12 @@
 import warnings
 from cryptography.utils import CryptographyDeprecationWarning
+from contextlib import closing
 
 with warnings.catch_warnings():
     warnings.filterwarnings('ignore', category=CryptographyDeprecationWarning)
     import paramiko
-    with paramiko.SSHClient() as client:
+
+    with closing(paramiko.SSHClient()) as client:
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect('192.168.1.215', 2222, username='lines14', key_filename='/home/lines14/.ssh/id_ed25519')
 
