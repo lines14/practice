ceb6033374519ef555ae9e21ba8830c2fc45c650
Lines14 <tonybass1414@yahoo.com>
2022-11-01 16:12:00 +0600

    Add "for" to readlines

--- Paramiko remote server connector.py
+++ Paramiko remote server connector.py
@@ -4,7 +4,8 @@ client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 client.connect('192.168.1.215', 2222, username='lines14', key_filename='/home/lines14/.ssh/id_ed25519')
 
-stdin, stdout, stderr = client.exec_command('ls -a')
-print(stdout.readlines())
+stdin, stdout, stderr = client.exec_command('ifconfig')
+for y in stdout.readlines():
+    print(y)
 
 client.close()
